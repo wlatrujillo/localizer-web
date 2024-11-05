673bbc5fee76cad68237bdc3cea93d1028564f98
Nelson Trujillo <nelson.trujillo@cobistopaz.com>
2024-11-05 11:00:51 -0500

    fix: login add material forms

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,24 +2,43 @@ import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../core/service/auth.service';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, FormBuilder, FormGroup,ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ CommonModule, RouterModule ],
+  imports: [ CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  test: Date = new Date();
   private toggleButton: any;
   private sidebarVisible: boolean;
   private nativeElement: Node;
 
-  constructor(private element: ElementRef, private router: Router, private authService: AuthService) {
+  test: Date = new Date();
+  loading: boolean = false;
+  submitted: boolean = false;
+  loginForm: FormGroup;
+
+  constructor(private element: ElementRef,
+              private router: Router,
+              private formBuilder: FormBuilder,
+              private authService: AuthService) {
     this.nativeElement = element.nativeElement;
     this.sidebarVisible = false;
+    this.loginForm = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]]
+    });
+
   }
+
+  // for accessing to form fields
+  get fval() { return this.loginForm.controls; }
 
   ngOnInit() {
     var navbar : HTMLElement = this.element.nativeElement;
@@ -31,8 +50,9 @@ export class LoginComponent implements OnInit, OnDestroy {
     setTimeout(function() {
       // after 1000 ms we add the class animated to the login/register card
       card.classList.remove('card-hidden');
-    }, 700);
-  }
+    }, 300);
+     }
+
   sidebarToggle() {
     var toggleButton = this.toggleButton;
     var body = document.getElementsByTagName('body')[0];
@@ -49,14 +69,22 @@ export class LoginComponent implements OnInit, OnDestroy {
       body.classList.remove('nav-open');
     }
   }
+
   ngOnDestroy(){
     const body = document.getElementsByTagName('body')[0];
     body.classList.remove('login-page');
     body.classList.remove('off-canvas-sidebar');
   }
 
-  onClickLogin(){
-    this.authService.login('wladimir.trujillo.ec@gmail.com','Passw0rd2024')
-    .subscribe(() => this.router.navigate(['/admin']));
+  onLoginSubmit(){
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.submitted = true;
+    this.loading = true;
+
+    this.authService.login(this.fval['email']['value'], this.fval['password']['value'])
+    .subscribe(() => { this.router.navigate(['/admin']); this.loading = false;}, error => {console.error(error); this.loading = false;});
   }
 }
