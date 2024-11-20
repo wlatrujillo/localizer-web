898a46bed288b5d7d7470350874b7880a97ccd5f
Nelson Trujillo <nelson.trujillo@cobistopaz.com>
2024-11-19 22:59:26 -0500

    feat: add sidenav

--- src/app/layouts/admin/admin-layout.component.ts
+++ src/app/layouts/admin/admin-layout.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule, Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../core/service/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [],
+  imports: [ MatSidenavModule, RouterModule, FormsModule, MatIconModule ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
 export class AdminLayoutComponent {
+
+  opened = true;
+
+  constructor(private router: Router,
+             private authService: AuthService) {
+
+  }
+
+  logout() {
+
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+
+  }
 
 }
