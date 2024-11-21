a36a15c56667fc02286c078faa23acb2a1a19b70
Nelson Trujillo <nelson.trujillo@cobistopaz.com>
2024-11-20 22:20:55 -0500

    feat: add projects

--- src/app/admin/projects/projects.component.ts
+++ src/app/admin/projects/projects.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { AdminService } from '../../core/service/admin.service';
+import { Project } from '../../core/model/project';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ MatCardModule, MatButtonModule, MatGridListModule ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
@@ -12,13 +16,16 @@ export class ProjectsComponent implements OnInit {
 
   constructor(private adminService: AdminService) { }
 
+  projects: Project[] = [];
+
   ngOnInit() {
     this.adminService.getAllProjects().subscribe(
       (data) => {
         console.log(data);
+        this.projects = data;
       },
       (error) => {
-        console.log(error);
+        console.error(error);
       }
     );
   }
